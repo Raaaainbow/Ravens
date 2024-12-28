076ad23107b3100c6908e7d3d5bb126038615ac4
Raaaainbow <Sebastian@tjr.dk>
2024-12-28 12:42:10 +0100

    Facts added

--- Ravens.java
+++ Ravens.java
@@ -6,23 +6,23 @@ import java.io.FileNotFoundException;
 
 public class Ravens {
     public static void main (String[] args) throws FileNotFoundException {
+        System.out.println("To recieve a random raven fact, please type \"yes\", if you wish to exit the program, type anything else.");
         factReader();
     }
 
     public static void factGiverInput(int counter, ArrayList <String> facts) {
-        System.out.println("to recieve a random raven fact, please type \"yes\", if you wish to exit the program, type anything else.");
-        int randNum = randomNumber(counter);
         Scanner console = new Scanner(System.in);
         String input = console.nextLine();
-        input.toLowerCase();
-        if (input.equals("yes")) {
-            System.out.println("Here is a random raven fact");
+        if (input.toLowerCase().equals("yes")) {
+            System.out.println("Here is a random raven fact:");
             System.out.println(facts.get(randomNumber(counter)));
+            System.out.println("-------------------------------");
+            System.out.print("Do you wish to recieve another fact? ");
             factGiverInput(counter, facts);
             console.close();
         } else {
-            console.close();
             System.out.println("Goodbye");
+            console.close();
         }
     }
 
@@ -42,10 +42,11 @@ public class Ravens {
             throw new FileNotFoundException("The file \"Facts.txt\" could not be found in the correct directory");
         }
         reader.close();
-
+        /*
         for (int i = 0; i < facts.size(); i++) {    // Prints all facts, mainly for debugging purposes
             System.out.println((i+1) + ": " + facts.get(i));
         }
+        */
         factGiverInput(counter, facts);
     }
 

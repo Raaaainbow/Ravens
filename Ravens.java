b4180ee62672f502414f372d49692572c907712c
Raaaainbow <Sebastian@tjr.dk>
2025-01-03 23:33:34 +0100

    Facts can now only be served once, hashmap added for faster lookup times

--- Ravens.java
+++ Ravens.java
@@ -1,40 +1,67 @@
 import java.util.Random;
 import java.util.Scanner;
-import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Set;
 import java.io.File;
 import java.io.FileNotFoundException;
 
 public class Ravens {
     public static void main (String[] args) throws FileNotFoundException {
-        System.out.println("To recieve a random raven fact, please type \"yes\", if you wish to exit the program, type anything else.");
+        System.out.println("To recieve a random raven fact, please type \"yes\", if you wish to exit the program type \"no\"");
         factReader();
     }
 
-    public static void factGiverInput(int counter, ArrayList <String> facts) {
-        Scanner console = new Scanner(System.in);
-        String input = console.nextLine();
-        if (input.toLowerCase().equals("yes")) {
-            System.out.println("Here is a random raven fact:");
-            System.out.println(facts.get(randomNumber(counter)));
-            System.out.println("-------------------------------");
-            System.out.print("Do you wish to recieve another fact? ");
-            factGiverInput(counter, facts);
-            console.close();
-        } else {
-            System.out.println("Goodbye");
+    public static void factGiverInput(int counter, HashMap<Integer, String> factsHash, Set<Integer> usedFacts) {
+        while (true) {
+            Scanner console = new Scanner(System.in);
+            String input = console.nextLine();
+            int randNum;
+            do {
+                randNum = randomNumber(counter);
+            } while (usedFacts.contains(randNum));
+
+            if (input.toLowerCase().equals("yes")) {
+                factFinder(randNum, factsHash, usedFacts);
+            }
+
+            if (usedFacts.size() == counter) {
+                System.out.println("This program only has " + counter + " raven facts");
+                System.out.println("If you wish to add more facts, please edit the file called \"Facts.txt\"");
+                console.close();
+                break;
+            }
+
+            if (input.toLowerCase().equals("no")) {
+                System.out.println("Goodbye");
+                console.close();
+                break;
+            }
             console.close();
         }
+    }
+
+    public static void factFinder(int randNum, HashMap<Integer, String> factsHash, Set<Integer> usedFacts) {
+            System.out.println("Here is a random raven fact:");
+            System.out.println(factsHash.get(randNum));
+            System.out.println("-------------------------------");
+            System.out.print("Do you wish to recieve another fact? ");
+            usedFacts.add(randNum);
+            System.out.println(usedFacts.size());
     }
 
     public static void factReader() throws FileNotFoundException {
         File file = new File("Facts.txt");      // Opens file change string if file changed 
         Scanner reader = new Scanner(file);     // Creates Scanner object to read the file
-        ArrayList <String> facts = new ArrayList<String>();     // Array list to store facts in memory
+        HashMap<Integer, String> factsHash = new HashMap<>();
+        Set<Integer> usedFacts = new HashSet<>();
+        // Implement HashMap for gettings facts, keys = {1,2,3,..}, facts in order
+        // Implement Hashset for checking if fact has appeared before
         int counter = 0;
 
         if (file.exists() && file.canRead()) {  // Checks if file is accessible
             while (reader.hasNextLine()) {
-                facts.add(reader.nextLine());   // Adds fact to list
+                factsHash.put(counter, reader.nextLine());
                 counter++;
             }
         } else {
@@ -47,7 +74,7 @@ public class Ravens {
             System.out.println((i+1) + ": " + facts.get(i));
         }
         */
-        factGiverInput(counter, facts);
+        factGiverInput(counter, factsHash, usedFacts);
     }
 
     public static int randomNumber (int counter) {
